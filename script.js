a59fd65ea062c6615749b0ba5d550dcdc3acaf8e
Lim-JongHyuk <whd1232132@gmail.com>
2024-11-03 21:35:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// 전단지를 보이게 하는 함수
+function showFlyer(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+// 전단지를 숨기는 함수
+function closeFlyer(id) {
+  document.getElementById(id).style.display = 'none';
+}
